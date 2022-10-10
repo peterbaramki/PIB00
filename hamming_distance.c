71f8b6d9a481e1e295a86131b59751cf5ed2fefc
peterbaramki <56185792+peterbaramki@users.noreply.github.com>
2022-10-10 18:04:08 +0300

    find hamming distance
    find the hamming distance between 2 integers.

---
+++ hamming_distance.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+ 
+int hammingDistance(int n1, int n2)
+{
+    int x = n1 ^ n2;
+    int distance = 0;
+ 
+    while (x > 0) {
+        distance += x & 1;
+        x >>= 1;
+    }
+ 
+    return distance;
+}
+ 
+int main()
+{
+    int x = 0;
+    
+    while (x != -1) 
+    {
+       int n1, n2;
+        scanf("%d", &n1);
+        
+        if (n1 == -1)
+        {
+            return 0;
+        }
+        
+        scanf("%d", &n2);
+        
+        printf("%d\n", hammingDistance(n1, n2));
+        
+    }
+    
+ }
